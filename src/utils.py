dff924a58c915117afd3c53ede5774b3d5952559
Andy7475 <Andy7475@users.noreply.github.com>
2024-10-03 10:49:23 +0100

    added fonts dir and support for ja fonts

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 import io
 import json
 import os
@@ -7,6 +8,9 @@ import sys
 from collections import defaultdict
 from typing import List, Set, Tuple
 
+import numpy as np
+import pycountry
+import requests
 import spacy
 import vertexai
 from anthropic import AnthropicVertex
@@ -20,10 +24,8 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table, TableStyle
 from vertexai.preview.vision_models import ImageGenerationModel
-import pycountry
+
 from src.config_loader import config
-import numpy as np
-import hashlib
 
 load_dotenv()  # so we can use environment variables for various global settings
 
@@ -100,6 +102,7 @@ def generate_story_image(story_plan):
 
 
 from typing import Dict, List, Tuple
+
 from pydub import AudioSegment
 
 
@@ -284,108 +287,220 @@ def extract_json_from_llm_response(response):
         return None
 
 
+def download_font(url, font_name):
+    response = requests.get(url)
+    if response.status_code == 200:
+        font_path = f"{font_name.replace(' ', '_')}.ttf"
+        font_path = os.path.join("../fonts", font_path)
+        with open(font_path, "wb") as f:
+            f.write(response.content)
+        return font_path
+    return None
+
+
 def create_pdf_booklet(story_data_dict, output_filename="pdf_booklet.pdf"):
-    # Register a Unicode font that supports Serbian characters
+    # Font setup
+    font_name, font_path = setup_font(config.TARGET_LANGUAGE)
 
-    FONT_NAME = "Roboto"
+    # Define styles
+    styles = define_styles(font_name)
 
-    font_path = os.path.join(os.path.dirname(__file__), "DejaVuSans.ttf")
-    pdfmetrics.registerFont(TTFont("DejaVuSans", font_path))
-
+    # Create document
     doc = SimpleDocTemplate(output_filename, pagesize=letter)
     elements = []
 
-    styles = getSampleStyleSheet()
-    title_style = styles["Heading1"]
-    title_style.alignment = 1  # Center alignment
-    title_style.fontName = FONT_NAME
-    subtitle_style = styles["Heading2"]
-    subtitle_style.alignment = 1  # Center alignment
-    subtitle_style.fontName = FONT_NAME
-
-    # Create a custom style for table cells
-    cell_style = ParagraphStyle(
-        "CellStyle",
-        parent=styles["Normal"],
-        fontSize=10,
-        leading=12,
-        alignment=TA_LEFT,
-        fontName=FONT_NAME,
-    )
-
-    elements.append(Paragraph("Comprehensive Story Translation", title_style))
+    # Add title
+    elements.append(Paragraph("Comprehensive Story Translation", styles["Title"]))
     elements.append(Spacer(1, 12))
 
+    # Process story data
     for story_part, data in story_data_dict.items():
-        # Add story part title
-        elements.append(Paragraph(story_part.capitalize(), subtitle_style))
-        elements.append(Spacer(1, 12))
+        elements.extend(process_story_part(story_part, data, styles))
 
-        # Process translated phrases
-        if "translated_phrase_list" in data:
-            elements.append(Paragraph("Translated Phrases", styles["Heading3"]))
-            phrases_data = [["English", "Target Language"]]  # Table header
-            for eng, swe in data["translated_phrase_list"]:
-                phrases_data.append(
-                    [Paragraph(eng, cell_style), Paragraph(swe, cell_style)]
-                )
-
-            phrases_table = Table(phrases_data, colWidths=[250, 250])
-            phrases_table.setStyle(
-                TableStyle(
-                    [
-                        ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                        ("ALIGN", (0, 0), (-1, -1), "LEFT"),
-                        ("FONTNAME", (0, 0), (-1, -1), FONT_NAME),
-                        ("FONTSIZE", (0, 0), (-1, 0), 14),
-                        ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                        ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-                        ("TEXTCOLOR", (0, 1), (-1, -1), colors.black),
-                        ("FONTSIZE", (0, 1), (-1, -1), 10),
-                        ("TOPPADDING", (0, 1), (-1, -1), 6),
-                        ("BOTTOMPADDING", (0, 1), (-1, -1), 6),
-                        ("GRID", (0, 0), (-1, -1), 1, colors.black),
-                        ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ]
-                )
-            )
-            elements.append(phrases_table)
-            elements.append(Spacer(1, 12))
-
-        # Process dialogue
-        if "dialogue" in data and "translated_dialogue" in data:
-            elements.append(Paragraph("Dialogue", styles["Heading3"]))
-            dialogue_data = [["English", "Target Language"]]  # Table header
-            for eng, swe in zip(data["dialogue"], data["translated_dialogue"]):
-                dialogue_data.append(
-                    [
-                        Paragraph(f"{eng['text']}", cell_style),
-                        Paragraph(f"{swe['text']}", cell_style),
-                    ]
-                )
-
-            dialogue_table = Table(dialogue_data, colWidths=[250, 250])
-            dialogue_table.setStyle(
-                TableStyle(
-                    [
-                        ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                        ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                        ("ALIGN", (0, 0), (-1, -1), "LEFT"),
-                        ("FONTNAME", (0, 0), (-1, -1), FONT_NAME),
-                        ("FONTSIZE", (0, 0), (-1, 0), 14),
-                        ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                        ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-                        ("TEXTCOLOR", (0, 1), (-1, -1), colors.black),
-                        ("FONTSIZE", (0, 1), (-1, -1), 10),
-                        ("TOPPADDING", (0, 1), (-1, -1), 6),
-                        ("BOTTOMPADDING", (0, 1), (-1, -1), 6),
-                        ("GRID", (0, 0), (-1, -1), 1, colors.black),
-                        ("VALIGN", (0, 0), (-1, -1), "TOP"),
-                    ]
-                )
-            )
-            elements.append(dialogue_table)
-            elements.append(Spacer(1, 24))  # Add extra space between story parts
-
+    # Build the document
     doc.build(elements)
+
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+
+
+# Language to font mapping
+LANGUAGE_FONT_MAP = {
+    "ja": {"name": "HeiseiMin-W3", "google": False},
+    "zh": {"name": "STSong-Light", "google": False},
+    "ko": {"name": "HYSMyeongJoStd-Medium", "google": False},
+    "ru": {"name": "DejaVuSerif", "google": False},
+    "sr": {"name": "DejaVuSerif", "google": False},
+    "en": {"name": "Roboto", "google": True},
+    "sv": {"name": "Roboto", "google": True},
+    # Add more languages as needed
+}
+
+
+def get_google_font(language_code):
+    # Map language codes to Google Font names
+
+    font_name = LANGUAGE_FONT_MAP.get(language_code, {"name": "Roboto"})[
+        "name"
+    ]  # Default to Roboto if language not found
+
+    # URL for Google Fonts API
+    api_url = f"https://fonts.googleapis.com/css?family={font_name.replace(' ', '+')}"
+
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        # Extract the TTF URL from the CSS
+        for line in response.text.split("\n"):
+            if ".ttf" in line:
+                ttf_url = line.split("url(")[1].split(")")[0]
+                return font_name, ttf_url
+
+    return None, None
+
+
+def setup_font(target_language):
+    font_info = LANGUAGE_FONT_MAP.get(
+        target_language, {"name": "Helvetica", "google": False}
+    )
+    font_name = font_info["name"]
+
+    if font_info["google"]:
+        font_name, font_url = get_google_font(target_language)
+        if font_name and font_url:
+            font_path = download_font(font_url, font_name)
+            if font_path:
+                pdfmetrics.registerFont(TTFont(font_name, font_path))
+                print(f"Registered Google font: {font_name}")
+                return font_name, font_path
+    else:
+        if font_name != "Helvetica":
+            try:
+                pdfmetrics.registerFont(UnicodeCIDFont(font_name))
+            except KeyError:
+                pdfmetrics.registerFont(TTFont(font_name, "../fonts/DejaVuSans.ttf"))
+            except Exception(f"Some font error with {font_name}"):
+                font_name = "Helvetica"
+
+            print(f"Registered CID font: {font_name}")
+        else:
+            print("Using default font: Helvetica")
+
+    return font_name, None
+
+
+def define_styles(font_name):
+    styles = getSampleStyleSheet()
+
+    # Modify existing styles
+    styles["Title"].alignment = 1
+    styles["Title"].fontName = font_name
+
+    styles["Heading2"].alignment = 1
+    styles["Heading2"].fontName = font_name
+
+    styles.add(
+        ParagraphStyle(
+            name="Subtitle", parent=styles["Heading2"], alignment=1, fontName=font_name
+        )
+    )
+    # Add new styles
+    styles.add(
+        ParagraphStyle(
+            name="TableHeader",
+            parent=styles["Normal"],
+            fontSize=14,
+            fontName=font_name,
+            textColor=colors.whitesmoke,
+        )
+    )
+
+    styles.add(
+        ParagraphStyle(
+            name="TableCell",
+            parent=styles["Normal"],
+            fontSize=10,
+            leading=12,
+            alignment=TA_LEFT,
+            fontName=font_name,
+        )
+    )
+    return styles
+
+
+def process_story_part(story_part, data, styles):
+    elements = []
+    elements.append(Paragraph(story_part.capitalize(), styles["Subtitle"]))
+    elements.append(Spacer(1, 12))
+
+    if "translated_phrase_list" in data:
+        elements.extend(create_phrase_table(data["translated_phrase_list"], styles))
+
+    if "dialogue" in data and "translated_dialogue" in data:
+        elements.extend(
+            create_dialogue_table(data["dialogue"], data["translated_dialogue"], styles)
+        )
+
+    elements.append(Spacer(1, 24))
+    return elements
+
+
+def create_phrase_table(phrases, styles):
+    elements = []
+    elements.append(Paragraph("Translated Phrases", styles["Subtitle"]))
+
+    data = [["English", "Target Language"]]
+    for eng, target in phrases:
+        data.append(
+            [
+                Paragraph(eng, styles["TableCell"]),
+                Paragraph(target, styles["TableCell"]),
+            ]
+        )
+
+    table = Table(data, colWidths=[250, 250])
+    table.setStyle(create_table_style(styles["TableCell"].fontName))
+
+    elements.append(table)
+    elements.append(Spacer(1, 12))
+    return elements
+
+
+def create_dialogue_table(dialogue, translated_dialogue, styles):
+    elements = []
+    elements.append(Paragraph("Dialogue", styles["Subtitle"]))
+
+    data = [["English", "Target Language"]]
+    for eng, target in zip(dialogue, translated_dialogue):
+        data.append(
+            [
+                Paragraph("{}".format(eng["text"]), styles["TableCell"]),
+                Paragraph("{}".format(target["text"]), styles["TableCell"]),
+            ]
+        )
+
+    table = Table(data, colWidths=[250, 250])
+    table.setStyle(create_table_style(styles["TableCell"].fontName))
+
+    elements.append(table)
+    return elements
+
+
+def create_table_style(font_name):
+    return TableStyle(
+        [
+            ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+            ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+            ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+            ("FONTNAME", (0, 0), (-1, -1), font_name),
+            ("FONTSIZE", (0, 0), (-1, 0), 14),
+            ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+            ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
+            ("TEXTCOLOR", (0, 1), (-1, -1), colors.black),
+            ("FONTSIZE", (0, 1), (-1, -1), 10),
+            ("TOPPADDING", (0, 1), (-1, -1), 6),
+            ("BOTTOMPADDING", (0, 1), (-1, -1), 6),
+            ("GRID", (0, 0), (-1, -1), 1, colors.black),
+            ("VALIGN", (0, 0), (-1, -1), "TOP"),
+        ]
+    )
